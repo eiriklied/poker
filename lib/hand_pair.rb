2044b9aec39df0e805d9d3b7a5acf8e4d89e9daa
Eirik Lied <eiriklied@gmail.com>
2010-01-08 15:45:46 +0100

    Spec for hand_pair running

--- lib/hand_pair.rb
+++ lib/hand_pair.rb
@@ -1,10 +1,24 @@
-class HandPair
+require 'hand'
 
-  def initialize(cards)
-  end
+class HandPair < Hand
 
   def matches
-    true
+    card_groups = {}
+    @cards.each do |card|
+      if card_groups.key? card.number_value
+        card_groups[card.number_value] += 1
+      else
+        card_groups[card.number_value] = 1
+      end
+    end
+    
+    card_groups.values.each do |val|
+      if val >= 2
+        return true
+      end
+    end
+    
+    false
   end
   
 end
--- spec/hand_pair_spec.rb
+++ spec/hand_pair_spec.rb
@@ -8,7 +8,11 @@ describe HandPair do
   it "should find a pair in hand" do
     pair = HandPair.new("AS AD 2C 3C 4C")
     pair.matches.should == true
-
+  end
+  
+  it "should not find a pair in a hand without pairs" do
+    pair = HandPair.new("AS 5D 2C 3C 4C")
+    pair.matches.should == false
   end
 
 end
