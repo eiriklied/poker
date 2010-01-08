876f7988ef8f0c6c4c6e75a65394a075bbf38e58
Eirik Lied <eiriklied@gmail.com>
2010-01-08 16:08:47 +0100

    refactored HandHighCard to extend Hand

--- lib/hand_high_card.rb
+++ lib/hand_high_card.rb
@@ -1,6 +1,16 @@
-class HandHighCard
+require 'hand'
+
+class HandHighCard < Hand
   def find_high_card cards
    sorted_cards = cards.sort {|x,y| y.number_value <=> x.number_value}
    sorted_cards[0]
+  end
+  
+  def matches?
+    true
+  end
+  
+  def to_s
+    "High card #{find_high_card(@cards)}"
   end
 end
